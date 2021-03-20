ef8ca774044c35b1229258e91b14e5de7980ce34
Brett Pasula <brettpasula@gmail.com>
2021-03-20 15:37:05 -0700

    add basic map + dependencies

--- js/geoMap.js
+++ js/geoMap.js
@@ -1,11 +1,21 @@
 class GeoMap {
 
     constructor() {
-  
+      this.initVis();
     }
   
+    // Create SVG area, initialize scales and axes
     initVis() {
-      // Create SVG area, initialize scales and axes
+      
+      // https://gis.stackexchange.com/questions/182442/whats-the-most-appropriate-way-to-load-mapbox-studio-tiles-in-leaflet
+      const map = L.map('map').setView([36.1408, 5.3536], 2);
+      L.Icon.Default.imagePath - "images/";
+      L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        zoomOffset: -1,
+        tileSize: 512,
+        attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        accessToken: 'sk.eyJ1IjoiYnJldHRwYXN1bGEiLCJhIjoiY2ttaThjenpqMGVyMDJzcmh6d2w5anQ2aiJ9.x43UBzwi3iRfsZSSb5ubIQ'
+      }).addTo(map);
     }
   
     updateVis() {
