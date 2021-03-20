41011991167b2f30cba143ea453b680b8f546cd1
jacquelineyin <yin.sangxu@gmail.com>
2021-03-20 13:06:28 -0700

    main: filter data of all irrelevant items first

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+// Initialize constants and global variables
+const indicators = new Indicators();
+
+let barChart, lineChart;
+
+//TODO: get rid of this after we implement for all selectable items:
+let selected = { area: "world", comparisonAreas: [], indicator: indicators.GDP_PER_CAPITA}
+
 /**
  * Load data from CSV file asynchronously and render charts
  */
  d3.csv('data/Dataset.csv').then(data => {
+    // Filter all irrelevant data
+    let indicatorsOfInterest = indicators.getAllIndicatorsOfInterest();
+    data = data.filter(d => indicatorsOfInterest.includes(d.IndicatorName));
+
     data.forEach(d => {
         /* TODO */
     });  
+
+    //Initialize views
+    barChart = new BarChart({
+      parentElement: '#barchart'
+    }, data, selected)
   });
