78eec0703068312d548df9be16dea79887bb5276
Amy <amy2400@students.cs.ubc.ca>
2021-03-20 18:11:10 -0700

    add year slider

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@
   });
 
   var map = new GeoMap();
+
+// Year slider
+var slider = d3
+  .sliderHorizontal()
+  .min(1960)
+  .max(2014)
+  .step(1)
+  .width(1400)
+  .tickFormat(d3.format('d'))
+  .ticks(25);
+
+d3.select('#slider')
+  .append('svg')
+  .attr('width', 1500)
+  .attr('height', 100)
+  .append('g')
+  .attr('transform', 'translate(10,10)')
+  .call(slider);
