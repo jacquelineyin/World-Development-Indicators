ade94e7759e4c5f5711826a4dcb3f15f0dc80017
amy2400 <amy2400@students.cs.ubc.ca>
2021-03-21 16:56:04 -0700

    add basic structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 /**
  * Load data from CSV file asynchronously and render charts
  */
- d3.csv('data/Dataset.csv').then(data => {
-    data.forEach(d => {
-        /* TODO */
-    }); 
+ let data, lineChart;
+
+ d3.csv('data/Dataset.csv').then(_data => {
+   data = _data;
+   _data.forEach(d => {
+
   });
+ 
+   // Initialize and render chart
+   lineChart = new LineChart({ parentElement: '#linechart' }, data);
+   lineChart.updateVis();
+ });
 
   var map = new GeoMap();
