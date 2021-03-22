82dc9ef5238363b198a9c368165035c8a73e4584
jacquelineyin <yin.sangxu@gmail.com>
2021-03-22 13:08:29 -0700

    selected: added in-line doc skeleton

--- js/selected.js
+++ js/selected.js
@@ -11,6 +11,10 @@ class Selected {
         this.updateAllSelectedAreas(this.area, this.comparisonAreas);
     }
 
+    /**
+     * Purpose:
+     * @param {Object} area = {region: "", country: ""}
+     */
     addComparisonArea(area) {
         if (!this.comparisonAreas.includes(area) && this.comparisonAreas.length < 5) {
             this.comparisonAreas.push(area);
@@ -22,6 +26,10 @@ class Selected {
         this.updateAllSelectedAreas(this.area, this.comparisonAreas);
     }
 
+    /**
+     * Purpose: 
+     * @param {Object} area = {region: "", country: ""} 
+     */
     removeComparisonArea(area) {
         let index = this.comparisonAreas.indexOf(area);
         if (index > -1) {
@@ -33,7 +41,7 @@ class Selected {
     }
 
     /**
-     * 
+     * Purpose:
      * @param {Object} area = {region: "", country: ""} 
      */
     setArea({region, country}) {
@@ -44,30 +52,43 @@ class Selected {
         this.updateAllSelectedAreas(this.area, this.comparisonAreas);
     }
 
-    // setArea(area) {
-    //     // this.area.region = !!!region ? region : "World";
-    //     // this.area.country = !!!country ? country : "";
-    //     this.area = area;
-    // }
-
+    /**
+     * Purpose:
+     * @param {string} indicator 
+     */
     setIndicator(indicator) {
         if (indicator) {
             this.indicator = indicator;
         }
     }
 
+    /**
+     * Purpose:
+     * @param {Integer} min = lowerBound year (format: YYYY) 
+     * @param {Integer} max = upperBound year (format: YYYY)
+     */
     setTimeInterval(min, max) {
         if (min && max) {
             this.timeInterval = {min, max};
         }
     }
 
+    /**
+     * Purpose:
+     * @param {Object} area = {region: "", country: ""} 
+     * @param {string} indicator 
+     * @param {Integer} minYear = lowerBound year (format: YYYY) 
+     * @param {Integer} maxYear = upperBound year (format: YYYY)
+     */
     setItems(area, indicator, minYear, maxYear) {
         this.setArea(area);
         this.setIndicator(indicator);
         this.setTimeInterval(minYear, maxYear);
     }
 
+    /**
+     * Purpose: 
+     */
     updateAllSelectedAreas(area, comparisonAreas) {
         let {region, country} = area;
  
