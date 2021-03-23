c963b76697fa7f847fe51c3f1cb0665bbbcd57f7
Brett Pasula <brettpasula@gmail.com>
2021-03-23 12:00:57 -0700

    move geoJson import to main

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 /**
  * Load data from CSV file asynchronously and render charts
  */
-let data, yearSlider;
+let data, yearSlider, map, selectedIndicator;
 
- d3.csv('data/Dataset.csv').then(_data => {
-   data = _data;
-   data.forEach(d => {
-
+  d3.csv('data/Dataset.csv').then(_data => {
+  data = _data;
+  data.forEach(d => { 
+    d.Value = +d.Value;
   });
+
+  selectedIndicator = new Indicators().POPULATION_DENSITY; // default
+  const filteredData = data.filter(d => d.IndicatorName == selectedIndicator);
 
   // Initialize and render chart
   yearSlider = new YearSlider({ parentElement: '#slider' }, data);
-  yearSlider.updateVis();
-});
 
-var map = new GeoMap();
+  d3.json("./data/countries.geojson").then(_data => { 
+    map = new GeoMap(filteredData, _data);
+    map.updateVis();
+  });
+});
