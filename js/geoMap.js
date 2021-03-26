709fa50f7d11fb83103a2d70c4eda1444763a8f3
Brett Pasula <brettpasula@gmail.com>
2021-03-26 13:02:11 -0700

    first draft choropleth

--- js/geoMap.js
+++ js/geoMap.js
@@ -1,29 +1,78 @@
 class GeoMap {
 
-    constructor() {
+    constructor(_data, _geoJsonData, _selected) {
+      this.data = _data;
+      this.geoJson = _geoJsonData;
+      this.countries = this.geoJson.features.map(d => d.properties.ISO_A3);
+      this.selected = _selected;
       this.initVis();
     }
   
     // Create SVG area, initialize scales and axes
     initVis() {
-      
-      const map = L.map('map').setView([36.1408, 5.3536], 2);
+
+      // Initialize map and retrieve raster layer
+      this.map = L.map('map').setView([36.1408, 5.3536], 2);
       L.Icon.Default.imagePath = "images/";
       L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         zoomOffset: -1,
         tileSize: 512,
         attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         accessToken: 'sk.eyJ1IjoiYnJldHRwYXN1bGEiLCJhIjoiY2ttaThjenpqMGVyMDJzcmh6d2w5anQ2aiJ9.x43UBzwi3iRfsZSSb5ubIQ'
-      }).addTo(map);
-      var vectorLayer = new L.GeoJSON.AJAX("./data/countries.geojson");
-      vectorLayer.addTo(map);
+      }).addTo(this.map);
+
+      // Initialize indicator scale
+      this.indicatorScale = d3.scaleLinear()
+        .range([0,1]);
     }
   
+    // Prepare data and scales
     updateVis() {
-      // Prepare data and scales
+      // Filter by selected years, selected indicator
+      var filteredData = this.data.filter(d => this.selected.selectedYears.includes(d.Year) && d.IndicatorName == this.selected.indicator);
+
+      // Aggregate data by country and calculate the mean
+      var groupedData = d3.rollup(filteredData, v => d3.mean(v, i => i.Value), d => d.CountryCode);
+
+      // Remove countries for which we do not have a corresponding vector tile, e.g. "WLD"
+      for (let country of groupedData.keys()) { 
+        if (!this.countries.includes(country)) { 
+          groupedData.delete(country);
+        }
+      } 
+
+      // Set indicator scale domain
+      this.indicatorScale.domain([d3.min(groupedData.values()), d3.max(groupedData.values())]);
+
+      // Add scaled indicator value to GeoJSON data for rendering
+      this.geoJson.features.forEach(d => {
+        d.properties.indicatorValue = this.indicatorScale(groupedData.get(d.properties.ISO_A3));
+      });
+      
+      this.renderVis();
     }
   
+    // Bind data to visual elements, update axes
     renderVis() {
-      // Bind data to visual elements, update axes
+      L.geoJson(this.geoJson, {style: this.styleFeature}).addTo(this.map);
+    }
+
+    static getTileColor(d) { 
+      return  d > 0.8 ? '#08519c' : 
+              d > 0.6 ? '#3182bd' : 
+              d > 0.4 ? '#6baed6' : 
+              d > 0.2 ? '#bdd7e7' : 
+              isNaN(d) ? '#808080' : 
+                        '#eff3ff';
+    }
+
+    styleFeature(feature) {
+      return { 
+        fillColor: GeoMap.getTileColor(feature.properties.indicatorValue),
+        fillOpacity: 0.7,
+        weight: 2, 
+        color: 'white',
+        dashArray: '3'
+      };
     }
   }
--- js/main.js
+++ js/main.js
@@ -7,10 +7,6 @@ let barChart, yearSlider, lineChart, data, filteredData;
 /**
  * Load data from CSV file asynchronously and render charts
  */
-let data, yearSlider, map, selectedIndicator;
-/**
- * Load data from CSV file asynchronously and render charts
- */
 const parseTime = d3.timeParse("%Y");
 d3.csv('data/Dataset.csv').then(_data => {
   let indicatorsOfInterest = indicators.getAllIndicatorsOfInterest();
@@ -25,78 +21,24 @@ d3.csv('data/Dataset.csv').then(_data => {
   //TODO: Testing purposes only. Get rid of it after finishing implementation of selectionItems
   setTestSelectedItems();
 
-  //Initialize views
+  // Load in GeoJSON data and initialize map view
+  d3.json("./data/countries.geojson").then(geoJsonData => { 
+    map = new GeoMap(data, geoJsonData, selected);
+    map.updateVis();
+  });
+
+  // Initialize bar chart
   barChart = new BarChart({
     parentElement: '#barchart'
   }, data, selected);
 
-
-    // Initialize and render time slider
-    yearSlider = new YearSlider({ parentElement: '#slider' }, data);
-
-
-  d3.csv('data/Dataset.csv').then(_data => {
-  data = _data;
-  data.forEach(d => { 
-    d.Value = +d.Value;
-  });
-
-
-  selectedIndicator = new Indicators().POPULATION_DENSITY; // default
-  const filteredData = data.filter(d => d.IndicatorName == selectedIndicator);
-
-  // Initialize and render chart
-  yearSlider = new YearSlider({ parentElement: '#slider' }, data);
-
-  d3.json("./data/countries.geojson").then(_data => { 
-    map = new GeoMap(filteredData, _data);
-    map.updateVis();
-  });
-});
-  var map = new GeoMap();
-  
-  
-  // ----------------- Helpers -------------------- //
-
-  /**
-   * Purpose: Returns an array of all the years in the given dataset
-   * @param {Array} data 
-   * @returns {Array} of Objects
-   */
-  let getAllYears = (data) => {
-    let years = data.map(d => d.Year);
-    return years;
-  }
-
-  /**
-   * Purpose: Creates a mock "selected" state for testing purposes
-   */
-  let setTestSelectedItems = () => {
-    // test value timeInterval
-    let years = getAllYears(data);
-    selected.timeInterval = {min: d3.min(years), max: d3.max(years)};
-
-
-    // test value focusArea
-    selected.setArea({region: "World", country: "Japan"});
-
-    // test value comparison countries
-    selected.addComparisonArea("Canada");
-    selected.addComparisonArea("China");
-    selected.addComparisonArea("Brazil");
-
-    // test value indicator
-    selected.setIndicator(indicators.MOBILE_CELLULAR_SUBSCRIPTIONS);
-  }
+  // Initialize line chart
   lineChart = new LineChart({ parentElement: '#linechart' }, data, selected);
   lineChart.updateVis();
 
   // Initialize and render time slider
   yearSlider = new YearSlider({ parentElement: '#slider' }, data);
-
 });
-
-var map = new GeoMap();
 
 // ----------------- Helpers -------------------- //
 
