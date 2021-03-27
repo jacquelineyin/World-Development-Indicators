cc661f251af045fd59892210da7bd82046ff0d68
yjackie <yjackie@students.cs.ubc.ca>
2021-03-26 21:20:14 -0700

    constants: add constant class for dispatcherEvents

---
+++ js/constants/dispatcherEvents.js
@@ -0,0 +1,7 @@
+class DispatcherEvents {
+    constructor() {
+        this.FILTER_YEAR = 'filterYear';
+        this.SELECT_FOCUS_AREA = 'selectFocusArea';
+        this.SELECT_COMPARISON_ITEM = 'selectComparisonItem';
+    }
+}
