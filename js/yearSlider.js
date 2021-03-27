a36b51b4efaecea8f53e7f672031bdb1e76f9974
amy2400 <amy2400@students.cs.ubc.ca>
2021-03-26 20:58:15 -0700

    variable name change

--- js/yearSlider.js
+++ js/yearSlider.js
@@ -38,8 +38,8 @@ class YearSlider {
         const maxYear = val.map(d3.format('d'))[1];
         const selectedYears = [];
 
-        for (let year = minYear; year <= maxYear; i++) {
-         selectedYears.push(i.toString());
+        for (let year = minYear; year <= maxYear; year++) {
+         selectedYears.push(year.toString());
        }
 
        vis.dispatcher.call('filterYear', this, selectedYears);
