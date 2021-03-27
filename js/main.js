5ca175d0c55f0fa656847e66bb78bdf4979dcc6c
yjackie <yjackie@students.cs.ubc.ca>
2021-03-26 18:08:39 -0700

    main: helpers to create selectFocusArea elems

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ d3.csv('data/Dataset.csv').then(_data => {
 
   //TODO: Testing purposes only. Get rid of it after finishing implementation of selectionItems
   setTestSelectedItems();
+  createFocusAreaSection();
 
   //Initialize views
   barChart = new BarChart({
@@ -43,6 +44,69 @@ d3.csv('data/Dataset.csv').then(_data => {
 var map = new GeoMap();
 
 // ----------------- Helpers -------------------- //
+
+let createFocusAreaSection = () => {
+  createSelectCountryDropdown();
+  createRegionRadioButtons();
+}
+
+let createSelectCountryDropdown = () => {
+  let countryList = regionMapper.getCountriesOfRegion(selected.area.region);
+  console.log(countryList);
+
+  let parent = document.getElementById("country-selector-container");
+  let select = document.createElement("select");
+  select.name = "country-selector";
+  select.id = select.name;
+
+  for (let country of countryList) {
+    let option = document.createElement("option");
+    option.value = country;
+
+    // Capitalize first letter
+    option.text = country.charAt(0).toUpperCase() + country.slice(1);
+
+    // Set default selected
+    if (country === countries.CANADA) {
+      option.selected = "selected";
+    }
+
+    select.appendChild(option);
+  }
+
+
+
+  // let label = document.createElement("label");
+  // label.innerHTML = "Select a Country: "
+  // label.htmlFor = "country-selector";
+
+  parent
+  // .appendChild(label)
+  .appendChild(select);
+}
+
+let createRegionRadioButtons = () => {
+  let regionList = regions.getAllRegions();
+
+  let parent = document.getElementById("region-selector-container");
+
+  for (let region of regionList) {
+    let radio = document.createElement("input");
+    radio.type = "radio";
+    radio.className = "radio-button";
+    radio.name = "region";
+    radio.id = `region-${region}`;
+    radio.value = region;
+  
+    let label = document.createElement("label");
+    label.className = "radio-label";
+    label.htmlFor = radio.id;
+    label.innerHTML = radio.value;
+
+    parent.appendChild(radio);
+    parent.appendChild(label);
+  }
+}
 
 /**
  * Purpose: Returns an array of all the years in the given dataset
