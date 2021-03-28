a54a2e567205aa43136c3334ad1d69b472364b6d
yjackie <yjackie@students.cs.ubc.ca>
2021-03-27 22:43:23 -0700

    code cleanup: single quote to match rest of project

--- js/constants/regions.js
+++ js/constants/regions.js
@@ -1,12 +1,12 @@
 class Regions {
     constructor() {
-        this.WORLD = "World",
-        this.AFRICA = "Africa",
-        this.ASIA = "Asia",
-        this.EUROPE = "Europe",
-        this.OCEANIA = "Oceania",
-        this.NORTH_AMERICA = "North America",
-        this.SOUTH_AMERICA = "South America"
+        this.WORLD = 'World',
+        this.AFRICA = 'Africa',
+        this.ASIA = 'Asia',
+        this.EUROPE = 'Europe',
+        this.OCEANIA = 'Oceania',
+        this.NORTH_AMERICA = 'North America',
+        this.SOUTH_AMERICA = 'South America'
     }
 
     getAllRegions() {
