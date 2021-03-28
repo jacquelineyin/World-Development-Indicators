4b380044ad2e88649bffd8eeef2dd01c68988c96
Brett Pasula <brettpasula@gmail.com>
2021-03-28 13:35:48 -0700

    fix logic, add to dispatcher

--- js/wedgeView.js
+++ js/wedgeView.js
@@ -23,7 +23,9 @@ class WedgeView {
       });
 
       // Initialize pie generator
-      this.pie = d3.pie(); 
+      this.pie = d3.pie()
+        .startAngle(0)
+        .endAngle(5); 
 
       // Intialize arc generators
       this.countryArc = d3.arc()
@@ -38,16 +40,16 @@ class WedgeView {
     // Prepare data and scales
     updateVis() {
       // Each wedge (indicator) needs three pieces of data from the subset of data within the range of selected years
-      // 1. maximum value for that indicator
-      // 2. average value for the selected country
-      // 3. average value for the world
-      var filteredToYearsData = this.data.filter(d => this.selected.selectedYears.includes(d.Year));
+      // 1. maximum value for that indicator (maxDataMap)
+      // 2. average value for the selected country (countryDataMap)
+      // 3. average value for the world (worldDataMap)
+      var filteredToYearsDataNoWLD = this.data.filter(d => this.selected.selectedYears.includes(d.Year) && d.CountryCode != "WLD");
       var filteredCountryData = this.data.filter(d => this.selected.selectedYears.includes(d.Year) && d.CountryName == "Canada");
-      var filteredWorldData = this.data.filter(d => this.selected.selectedYears.includes(d.Year) && d.CountryCode == "WLD");
       // https://stackoverflow.com/questions/4020796/finding-the-max-value-of-an-attribute-in-an-array-of-objects
-      this.maxDataMap = d3.rollup(filteredToYearsData, v => Math.max.apply(Math, v.map((o) => o.Value)), d => d.IndicatorName);
+      this.maxDataMap = d3.rollup(filteredToYearsDataNoWLD, v => Math.max.apply(Math, v.map((o) => o.Value)), d => d.IndicatorName);
       this.countryDataMap = d3.rollup(filteredCountryData, v => d3.mean(v, i => i.Value), d => d.IndicatorName);
-      this.worldDataMap = d3.rollup(filteredWorldData, v => d3.mean(v, i => i.Value), d => d.IndicatorName);
+      this.worldDataMap = d3.rollup(filteredToYearsDataNoWLD, v => d3.mean(v, i => i.Value), d => d.IndicatorName);
+      console.log(this.worldDataMap);
       this.renderVis();
     }
   
@@ -58,28 +60,34 @@ class WedgeView {
         var max = this.maxDataMap.get(this.indicators[d]);
         var countryAvg = this.countryDataMap.get(this.indicators[d]);
         var worldAvg = this.worldDataMap.get(this.indicators[d]);
+        
+        // Country
         if (max && countryAvg) {
         var countryData = [countryAvg, (max - countryAvg)];
         // https://stackoverflow.com/questions/24118919/how-do-i-get-the-index-number-from-the-array-in-d3/24118970
         d3.select('#' + d + ' svg g')
-          .selectAll('c-arc')
-          .data(this.pie(countryData))
-          .enter()
-          .append('path')
-          .attr('class', 'c-arc')
-          .attr('num', function(d,i){return i;})
-          .attr('d', this.countryArc);
+          .selectAll('.c-arc')
+          .data(this.pie(countryData), d => d.data)
+          .join(
+            enter => enter.append('path')
+              .attr('class', 'c-arc')
+              .attr('num', d => d.index)
+              .attr('d', this.countryArc),
+          exit => exit.remove())
         }
+
+        // World
         if (max && worldAvg) {
         var worldData = [worldAvg, (max - worldAvg)];
         d3.select('#' + d + ' svg g')
-          .selectAll('w-arc')
-          .data(this.pie(worldData))
-          .enter()
-          .append('path')
-          .attr('class', 'w-arc')
-          .attr('num', function(d,i){return i;})
-          .attr('d', this.worldArc);
+          .selectAll('.w-arc')
+          .data(this.pie(worldData), d => d.data)
+          .join(
+            enter => enter.append('path')
+              .attr('class', 'w-arc')
+              .attr('num', d => d.index)
+              .attr('d', this.worldArc),
+            exit => exit.remove())
         }
       });
     }
