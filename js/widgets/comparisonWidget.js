0131c9978118263b395ba76d03d0cfa6e59dbf7e
yjackie <yjackie@students.cs.ubc.ca>
2021-03-29 16:05:58 -0700

    comparison widget: first draft of autocomplete (from w3school)

--- js/widgets/comparisonWidget.js
+++ js/widgets/comparisonWidget.js
@@ -49,11 +49,25 @@ class ComparisonWidget {
         // Create container div
         let div = document.createElement('div');
         div.className = 'container';
+
+        // Create autocomplete div
+        let autocomplete = document.createElement('div');
+        autocomplete.className = 'autocomplete';
         
-        // Create input
+        // Create input field
         let input = document.createElement('input');
-        input.placeholder = 'Add country to compare...'
+        input.placeholder = 'Add country to compare...';
+        input.autocomplete = false;
         input.id = 'comparison-input';
+
+        // Workaround to remove chrome's autocomplete
+        input.readOnly = true;
+        input.onfocus = () => {
+            input.removeAttribute('readOnly');
+        }
+
+        // Autocomplete dropdown functionality
+        this.autocomplete(input, this.regionMapper.getCountriesOfRegion(this.regions.WORLD))
 
         // Create submit button
         let submitButton = document.createElement('button');
@@ -64,7 +78,9 @@ class ComparisonWidget {
         submitButton.id = submitButton.name;
         submitButton.addEventListener('click', e => this.handleSubmitInput(e))
 
-        div.appendChild(input);
+        autocomplete.appendChild(input);
+
+        div.appendChild(autocomplete);
         div.appendChild(submitButton);
 
         parent.appendChild(div);
@@ -148,4 +164,105 @@ class ComparisonWidget {
           parentNode.firstChild.remove();
         }
     }
+
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+     autocomplete(inp, arr) {
+        var currentFocus;
+        /*execute a function when someone writes in the text field:*/
+        inp.addEventListener("input", function(e) {
+            let a, b, i, val;
+            val = this.value;
+
+            /*close any already open lists of autocompleted values*/
+            closeAllLists();
+
+            if (!val) { return false;}
+
+            currentFocus = -1;
+            /*create a DIV element that will contain the items (values):*/
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+            /*append the DIV element as a child of the autocomplete container:*/
+            this.parentNode.appendChild(a);
+            /*for each item in the array...*/
+            for (i = 0; i < arr.length; i++) {
+              /*check if the item starts with the same letters as the text field value:*/
+              if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                    b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+              }
+            }
+        });
+        /*execute a function presses a key on the keyboard:*/
+        inp.addEventListener("keydown", function(e) {
+            var x = document.getElementById(this.id + "autocomplete-list");
+            if (x) x = x.getElementsByTagName("div");
+            if (e.keyCode == 40) {
+              /*If the arrow DOWN key is pressed,
+              increase the currentFocus variable:*/
+              currentFocus++;
+              /*and and make the current item more visible:*/
+              addActive(x);
+            } else if (e.keyCode == 38) { //up
+              /*If the arrow UP key is pressed,
+              decrease the currentFocus variable:*/
+              currentFocus--;
+              /*and and make the current item more visible:*/
+              addActive(x);
+            } else if (e.keyCode == 13) {
+              /*If the ENTER key is pressed, prevent the form from being submitted,*/
+              e.preventDefault();
+              if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+              }
+            }
+        });
+        function addActive(x) {
+          /*a function to classify an item as "active":*/
+          if (!x) return false;
+          /*start by removing the "active" class on all items:*/
+          removeActive(x);
+          if (currentFocus >= x.length) currentFocus = 0;
+          if (currentFocus < 0) currentFocus = (x.length - 1);
+          /*add class "autocomplete-active":*/
+          x[currentFocus].classList.add("autocomplete-active");
+        }
+        function removeActive(x) {
+          /*a function to remove the "active" class from all autocomplete items:*/
+          for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+          }
+        }
+        function closeAllLists(elmnt) {
+          /*close all autocomplete lists in the document,
+          except the one passed as an argument:*/
+          var x = document.getElementsByClassName("autocomplete-items");
+          for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+            x[i].parentNode.removeChild(x[i]);
+          }
+        }
+      }
+      /*execute a function when someone clicks in the document:*/
+      document.addEventListener("click", function (e) {
+          closeAllLists(e.target);
+      });
+      }
 }
