d67209df5860f1c3f8821e261cf6bbc1f0e9d763
yjackie <yjackie@students.cs.ubc.ca>
2021-03-30 00:03:30 -0700

    code cleanup: single quote styling

--- js/widgets/autocomplete.js
+++ js/widgets/autocomplete.js
@@ -4,26 +4,33 @@
  */
 class AutocompleteCreator {
 
+    /**
+     * Class constructor
+     * @param {Object} _inputElem = DOM node : input element 
+     * @param {Object} _submitButton = DOM node : button
+     */
     constructor(_inputElem, _submitButton) {
         this.keyEventMapper = new KeyEventMapper();
         this.inputElem = _inputElem;
         this.submitButton = _submitButton;
     }
 
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
+    
+    /**
+     * Purpose: Creates a custom autocomplete dropdown using given array
+     * @param {Array} arr : array of possible autocompleted values
+     */
     autocomplete(arr) {
-        let autoCreator = this;
-        let {inputElem} = autoCreator;
+        let {inputElem} = this;
         let indexOfFocus;
 
         /*execute a function when someone writes in the text field:*/
-        inputElem.addEventListener("input", function(e) {
+        inputElem.addEventListener('input', (e) => {
             // let itemContainer, item, index;
-            let val = this.value;
+            let val = inputElem.value;
 
             /*close any already open lists of autocompleted values*/
-            autoCreator.closeAllLists();
+            this.closeAllLists();
 
             if (!val) { return false;}
 
@@ -31,25 +38,25 @@ class AutocompleteCreator {
             indexOfFocus = -1;
             
             /*create a DIV element that will contain the items (values):*/
-            const itemContainer = autoCreator.createItemContainer();
+            const itemContainer = this.createItemContainer();
 
             /*for each item in the array...*/
-            autoCreator.createItems(arr, autoCreator, val, itemContainer);
+            this.createItems(arr, val, itemContainer);
         });
         
         /*execute a function presses a key on the keyboard:*/
-        inputElem.addEventListener("keydown", function(e) {
+        inputElem.addEventListener('keydown', (e) => {
             let itemContainer = document.getElementById(inputElem.id + '-autocomplete-list');
             let items;
 
-            if (itemContainer) items = itemContainer.getElementsByTagName("div");
+            if (itemContainer) items = itemContainer.getElementsByTagName('div');
 
-            indexOfFocus = autoCreator.handleKeyEvent(e, indexOfFocus, items)
+            indexOfFocus = this.handleKeyEvent(e, indexOfFocus, items)
         });
 
       /*execute a function when someone clicks in the document:*/
-      document.addEventListener("click", function (e) {
-          autoCreator.closeAllLists(e.target);
+      document.addEventListener('click', (e) => {
+          this.closeAllLists(e.target);
         });
       }
 
@@ -84,12 +91,13 @@ class AutocompleteCreator {
     handleEnterKey(e, index, items) {
         e.preventDefault();
         if (index > -1 && items) {
-            /*and simulate a click on the "active" item:*/
+            /*and simulate a click on the 'active' item:*/
             items[index].click();
         } else {
             this.submitButton.click();
             index = -1;
         }
+
         return index;
     }
 
@@ -100,7 +108,7 @@ class AutocompleteCreator {
     closeAllLists(clickedElem) {
         /*close all autocomplete lists in the document,
         except the one passed as an argument:*/
-        let autocompleteItems = document.getElementsByClassName("autocomplete-items");
+        let autocompleteItems = document.getElementsByClassName('autocomplete-items');
 
         for (let i = 0; i < autocompleteItems.length; i++) {
             let isClickAutoItem = clickedElem === autocompleteItems[i];
@@ -113,35 +121,35 @@ class AutocompleteCreator {
     }
 
 
-    /*a function to classify an item as "active":*/
+    /*a function to classify an item as 'active':*/
     addActive(items, index) {
         if (!items) return false;
 
-        /*start by removing the "active" class on all items:*/
+        /*start by removing the 'active' class on all items:*/
         this.removeActive(items);
 
         // Adjust index in a modular fashion that handles negative values
         if (index >= items.length) index = 0;
         if (index < 0) index = (items.length - 1);
 
-        /*add class "autocomplete-active":*/
-        items[index].classList.add("autocomplete-active");
+        /*add class 'autocomplete-active':*/
+        items[index].classList.add('autocomplete-active');
         return index;
       }
 
     removeActive(items) {
-        /*a function to remove the "active" class from all autocomplete items:*/
+        /*a function to remove the 'active' class from all autocomplete items:*/
         for (var i = 0; i < items.length; i++) {
-          items[i].classList.remove("autocomplete-active");
+          items[i].classList.remove('autocomplete-active');
         }
       }
 
 
-    createItems(arr, autoCreator, val, itemContainer) {
+    createItems(arr, val, itemContainer) {
         let index;
         for (index = 0; index < arr.length; index++) {
             /*check if the item starts with the same letters as the text field value:*/
-            if (autoCreator.checkIfSameString(arr, index, val)) {
+            if (this.checkIfSameString(arr, index, val)) {
                 /*create a DIV element for each matching element:*/
                 this.createItem(arr, index, val, itemContainer);
             }
@@ -160,9 +168,9 @@ class AutocompleteCreator {
         item.innerHTML += `<input type='hidden' value='${arr[index]}'>`;
         
         /*execute a function when someone clicks on the item value (DIV element):*/
-        item.addEventListener("click", function (e) {
+        item.addEventListener('click', function (e) {
             /*insert the value for the autocomplete text field:*/
-            autoCreator.inputElem.value = this.getElementsByTagName("input")[0].value;
+            autoCreator.inputElem.value = this.getElementsByTagName('input')[0].value;
 
             autoCreator.closeAllLists();
         });
@@ -179,8 +187,8 @@ class AutocompleteCreator {
 
     createItemContainer() {
         const itemContainer = document.createElement('div');
-        itemContainer.setAttribute("id", this.inputElem.id + "-autocomplete-list");
-        itemContainer.setAttribute("class", "autocomplete-items");
+        itemContainer.setAttribute('id', this.inputElem.id + '-autocomplete-list');
+        itemContainer.setAttribute('class', 'autocomplete-items');
         
         /*append the DIV element as a child of the autocomplete container:*/
         this.inputElem.parentNode.appendChild(itemContainer);
